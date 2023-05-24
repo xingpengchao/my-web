a7c8fffd52d4437056beb40c9a804cf993c25368
allenxing <2659489932@qq.com>
2023-05-24 22:58:37 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,23 @@
+## 平台简介
+
+* 基于Ruiyi-Vue3改造的个人网站
+
+## 前端运行
+
+```bash
+# 克隆项目
+git clone 仓库地址
+
+# 进入项目目录
+cd RuoYi-Vue3
+
+# 安装依赖
+yarn --registry=https://registry.npmmirror.com
+
+# 启动服务
+yarn dev
+
+# 构建测试环境 yarn build:stage
+# 构建生产环境 yarn build:prod
+# 前端访问地址 http://localhost:80
+```
---
+++ src/views/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="index">
+    <el-row>
+      <el-button>Default</el-button>
+      <el-button class="custom" type="primary" @click="goTarget(url)">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </el-row>
+  </div>
+</template>
+
+<script setup name="Index">
+const url = ref('http://www.baidu.com')
+
+function goTarget(url) {
+  window.open(url, '__blank')
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/styles/variables.module.scss";
+
+.index {
+  background: #ffffff;
+  min-height: 100vh;
+  .custom {
+    height: px2vh(120);
+    width: px2vw(200);
+  }
+}
+</style>
+
